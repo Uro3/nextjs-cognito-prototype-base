267d1ecb77d470e1f41dd5487f737a1fe39ba939
Uro3 <miyakawa.uro3@gmail.com>
2025-03-19 11:23:36 +0900

    コードフォーマット修正

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,12 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-2xl font-semibold mb-2">ようこそ！プロトタイプアプリへ！</h1>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">ログイン</button>
+      <h1 className="text-2xl font-semibold mb-2">
+        ようこそ！プロトタイプアプリへ！
+      </h1>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        ログイン
+      </button>
     </div>
   );
 }
