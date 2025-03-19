e12a2c1a7ad808ebae5e347d3d2c0395269e6a35
Uro3 <miyakawa.uro3@gmail.com>
2025-03-19 11:58:14 +0900

    Cognitoプロバイダーを追加

--- web/src/types/env.d.ts
+++ web/src/types/env.d.ts
@@ -2,6 +2,9 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       AUTH_SECRET: string;
+      AUTH_COGNITO_ID: string;
+      AUTH_COGNITO_ISSUER: string;
+      AUTH_COGNITO_SECRET: string;
     }
   }
 }
